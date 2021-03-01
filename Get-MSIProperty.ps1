73d1b030b78626537ae6957eea1a4875708267c4
Andrey Vyporkhanyuk <andrey.vyporkhanyuk@outlook.com>
2021-03-01 18:47:59 +0200

    add garbige collection to release msi file

--- Get-MSIProperty.ps1
+++ Get-MSIProperty.ps1
@@ -1,39 +1,37 @@
-param(
-	[parameter(Mandatory=$true)]
-	[ValidateNotNullOrEmpty()]
-	[System.IO.FileInfo]$Path,
- 
-	[parameter(Mandatory=$true)]
-	[ValidateNotNullOrEmpty()]
-	[ValidateSet("ProductCode", "ProductVersion", "ProductName", "Manufacturer", "ProductLanguage", "FullVersion")]
-	[string]$Property
-)
-Process {
-	try {
-		# Read property from MSI database
-		$WindowsInstaller = New-Object -ComObject WindowsInstaller.Installer
-		$MSIDatabase = $WindowsInstaller.GetType().InvokeMember("OpenDatabase", "InvokeMethod", $null, $WindowsInstaller, @($Path.FullName, 0))
-		$Query = "SELECT Value FROM Property WHERE Property = '$($Property)'"
-		$View = $MSIDatabase.GetType().InvokeMember("OpenView", "InvokeMethod", $null, $MSIDatabase, ($Query))
-		$View.GetType().InvokeMember("Execute", "InvokeMethod", $null, $View, $null)
-		$Record = $View.GetType().InvokeMember("Fetch", "InvokeMethod", $null, $View, $null)
-		$Value = $Record.GetType().InvokeMember("StringData", "GetProperty", $null, $Record, 1)
- 
-		# Commit database and close view
-		$MSIDatabase.GetType().InvokeMember("Commit", "InvokeMethod", $null, $MSIDatabase, $null)
-		$View.GetType().InvokeMember("Close", "InvokeMethod", $null, $View, $null)           
-		$MSIDatabase = $null
-		$View = $null
- 
-		# Return the value
-		return $Value
-	} 
-	catch {
-		Write-Warning -Message $_.Exception.Message ; break
-	}
-}
-End {
-	# Run garbage collection and release ComObject
-	[System.Runtime.Interopservices.Marshal]::ReleaseComObject($WindowsInstaller) | Out-Null
-	[System.GC]::Collect()
+Function Get-MsiProps () { 
+    param ( [Parameter(Mandatory)] [String] $FilePath )
+
+    # open msi in read only mode
+    $READONLY = 0
+    $WindowsInstaller = New-Object -ComObject WindowsInstaller.Installer
+    $msidb = $WindowsInstaller.OpenDatabase("$FilePath", $READONLY)
+
+    # read property table
+    $queryString = 'SELECT * FROM `Property`'
+    $PropertyTable = $msidb.OpenView($queryString)
+    $PropertyTable.Execute()
+
+    # read each property name and value row-by-row
+    $PROP_NAME_ID = 1
+    $PROP_VALUE_ID = 2
+    $Props = @{ }
+    do {
+        $Property = $PropertyTable.Fetch()
+        If ($null -eq $Property) { break }
+        $PropName = $Property.StringData($PROP_NAME_ID)
+        $PropVal = $Property.StringData($PROP_VALUE_ID)
+        $Props[$PropName] = $PropVal
+    } while ($true)
+
+    $msidb.Commit
+    $PropertyTable.Close
+
+    $msidb = $null
+    $PropertyTable = $null
+    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($WindowsInstaller) | Out-Null
+    [System.GC]::Collect()
+    #[System.GC]::WaitForPendingFinalizers()
+
+    # return result as ps object
+    New-Object -TypeName PSObject -Property $Props
 }

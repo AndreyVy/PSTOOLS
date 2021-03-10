af45c26de8e4b48ac3ad9a3facf2217d14a0d1c4
Andrey Vyporkhanyuk <andrey.vyporkhanyuk@outlook.com>
2021-03-10 18:27:14 +0200

    remove -a parameter for netstat. Output shows empty values and ConvertFrom-String cannot deal with it properly

--- Get-NetStat.ps1
+++ Get-NetStat.ps1
@@ -6,15 +6,15 @@
     BEGIN{ }
     PROCESS{
         do {
-            netstat -a -n -o | Select-Object -Skip 4 | 
+            netstat -n -o | Select-Object -Skip 4 | 
                         ConvertFrom-String -PropertyNames Blank, Protocol,LocalAddress, ForeignAddress, State, PID |
                         Select-Object Protocol,
                                     @{name='LocalAddress'; expression={($_.LocalAddress).Substring(0,($_.LocalAddress).LastIndexOf(':') )}},
                                     @{name='LocalPort'; expression={(($_.LocalAddress) -split ":")[-1]}},
                                     @{name='ForeignAddress'; expression={($_.ForeignAddress).Substring(0,($_.ForeignAddress).LastIndexOf(':') )}},
-                                    @{'Name'='ForeignPort'; expression={(($_.ForeignAddress) -split ":")[-1]}},
+                                    @{name='ForeignPort'; expression={(($_.ForeignAddress) -split ":")[-1]}},
                                     State,
-                                    @{'Name'='ProcessName'; expression={If ($_.PID) {(Get-Process -Id $_.PID).Name} else {$Null}}}
+                                    @{name='ProcessName'; expression={If ($_.PID) {(Get-Process -Id $_.PID).Name} else {$Null}}}
         } while ($infitiny)
         
     }
